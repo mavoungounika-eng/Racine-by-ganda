fcdb57980aba18b0d9a904925a269727e05c98e0
mavoungounika-eng <manvoungounika@gmail.com>
2025-12-30 02:52:49 +0000

    chore(repo): harden gitignore for Laravel backend

--- Racine by GANDA/admin/includes/header.php
+++
@@ -1,9 +0,0 @@
-<?php if (!isset($_SESSION['admin'])) { header('Location: ../login.php'); exit; } ?>
-<nav>
-    <a href="dashboard.php">🏠 Tableau de bord</a> |
-    <a href="produits.php">🛍️ Produits</a> |
-    <a href="commandes.php">📦 Commandes</a> |
-    <a href="logout.php">🔓 Déconnexion</a>
-    <a href="../../index.php">Acceuil</a>
-</nav>
-<hr>
