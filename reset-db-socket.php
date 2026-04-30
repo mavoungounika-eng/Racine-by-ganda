bf4d2224c86491480357366dc3c3f83ef007f747
Nika <nika@racine.dev>
2026-04-30 02:54:18 +0100

    fix: remove sensitive files

--- reset-db-socket.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Script de réinitialisation de la base de données avec socket
- * Exécutez: php reset-db-socket.php
- */
-
-try {
-    echo " 🔄 Réinitialisation de la base de données...\n;
