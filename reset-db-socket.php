9de511724c4c4f2844e38e1e64054dc6dd5a1fbb
Nika <nika@racine.dev>
2026-04-30 02:50:08 +0100

    test ultrareview

---
+++ reset-db-socket.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Script de réinitialisation de la base de données avec socket
+ * Exécutez: php reset-db-socket.php
+ */
+
+try {
+    echo " 🔄 Réinitialisation de la base de données...\n;
